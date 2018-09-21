6746289718b7afe5334e08e8052c938bc5766e69
Wallacy Braz <wallacy.braz@live.com>
2018-09-21 14:28:46 -0300

    calculo imposto de renda - barra vermelha
    Obtida por meio de criação de novo teste

--- src/com/wallacy/calculadoraRentabilidadeTeste/CalculoImpostoRendaTeste.java
+++ src/com/wallacy/calculadoraRentabilidadeTeste/CalculoImpostoRendaTeste.java
@@ -11,7 +11,13 @@ public class CalculoImpostoRendaTeste {
     @Test
     public void testeCalculaImpostoRenda() {
 	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(60, 1000.0f, 8.5f);
-	assertEquals(calculadoraRentabilidade.getImpostoRenda(), 3.14f, 1f);
+	assertEquals(calculadoraRentabilidade.getImpostoRenda(), 3.14f, 0.05f);
+    }
+
+    @Test
+    public void testeCalculaImpostoRendaOutro() {
+	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(120, 500.0f, 8.0f);
+	assertEquals(calculadoraRentabilidade.getImpostoRenda(), 2.96f, 0.05f);
     }
 
 }
