42b0982f2e294e89445bb05aeccbd362bd730fdc
Wallacy Braz <wallacy.braz@live.com>
2018-09-21 14:41:19 -0300

    impost de renda - barra verde
    Obtida por 3º teste e triangulação

--- src/com/wallacy/calculadoraRentabilidadeTeste/CalculoImpostoRendaTeste.java
+++ src/com/wallacy/calculadoraRentabilidadeTeste/CalculoImpostoRendaTeste.java
@@ -2,22 +2,53 @@ package com.wallacy.calculadoraRentabilidadeTeste;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.wallacy.calculadoraRentabilidade.CalculadoraRentabilidade;
 
+@RunWith(Parameterized.class)
 public class CalculoImpostoRendaTeste {
+
+    @Parameters
+    public static Collection<Object[]> getParameters() {
+
+	Object[] parametro1 = new Object[] { 60, 1000.0f, 8.5f, 3.14f };
+
+	Object[] parametro2 = new Object[] { 120, 500.0f, 8.0f, 2.96f };
+
+	Object[] parametro3 = new Object[] { 240, 3000.0f, 9.0f, 35.51f };
+
+	return Arrays.asList(new Object[][] { parametro1, parametro2, parametro3 });
+
+    }
+
+    public CalculoImpostoRendaTeste(int diasEntrada, float aplicacaoInicialEntrada, float taxaJurosEntrada,
+	    float impostoEsperado) {
+	this.diasEntrada = diasEntrada;
+	this.aplicacaoInicialEntrada = aplicacaoInicialEntrada;
+	this.taxaJurosEntrada = taxaJurosEntrada;
+	this.impostoEsperado = impostoEsperado;
+    }
+
+    private int diasEntrada;
+
+    private float aplicacaoInicialEntrada;
+
+    private float taxaJurosEntrada;
+
+    private float impostoEsperado;
 
     @Test
     public void testeCalculaImpostoRenda() {
-	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(60, 1000.0f, 8.5f);
-	assertEquals(calculadoraRentabilidade.getImpostoRenda(), 3.14f, 0.05f);
-    }
-
-    @Test
-    public void testeCalculaImpostoRendaOutro() {
-	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(120, 500.0f, 8.0f);
-	assertEquals(calculadoraRentabilidade.getImpostoRenda(), 2.96f, 0.05f);
+	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(diasEntrada,
+		aplicacaoInicialEntrada, taxaJurosEntrada);
+	assertEquals(calculadoraRentabilidade.getImpostoRenda(), impostoEsperado, 0.05f);
     }
 
 }
