9070114f709dcb1afa05705deb19c404e648ee75
Wallacy Braz <wallacy.braz@live.com>
2018-09-21 13:33:07 -0300

    rendimento bruto teste - Barra verde
    Obtida por meio de implementação de unidade e primeira falsificação

---
+++ src/com/wallacy/calculadoraRentabilidade/CalculadoraRentabilidade.java
@@ -0,0 +1,21 @@
+package com.wallacy.calculadoraRentabilidade;
+
+public class CalculadoraRentabilidade {
+
+    private int dias;
+
+    private float aplicacaoInicial;
+
+    private float taxaJuros;
+
+    public CalculadoraRentabilidade(int dias, float aplicacaoInicial, float taxaJuros) {
+	this.dias = dias;
+	this.aplicacaoInicial = aplicacaoInicial;
+	this.taxaJuros = taxaJuros;
+    }
+
+    public float getRendimentoBruto() {
+	return 13.97f;
+    }
+
+}
--- src/com/wallacy/calculadoraRentabilidadeTeste/RendimentoBrutoTeste.java
+++ src/com/wallacy/calculadoraRentabilidadeTeste/RendimentoBrutoTeste.java
@@ -4,11 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.wallacy.calculadoraRentabilidade.CalculadoraRentabilidade;
+
 public class RendimentoBrutoTeste {
 
     @Test
     public void TestecalculaRendimentoBruto() {
-	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(60, 1000f, 8.5);
+	CalculadoraRentabilidade calculadoraRentabilidade = new CalculadoraRentabilidade(60, 1000f, 8.5f);
 	assertEquals(13.97f, calculadoraRentabilidade.getRendimentoBruto(), 0f);
     }
 
